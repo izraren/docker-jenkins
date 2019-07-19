c3fea06be172867491fd35a6f0378f2738ea9b79
izraren <izraren.zina@gmail.com>
2019-07-19 11:06:48 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import socket
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "<h1><center>Hello Dakar Innovation Day V1469.</center></h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=80)
